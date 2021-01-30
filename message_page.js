62df8383217bef5a7c97ee7e20615c4401873070
vanyagoyal <73106041+vanyagoyal@users.noreply.github.com>
2021-01-30 18:48:23 +0530

    Add files via upload

---
+++ message_page.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBhXfd2l0fnFDzabH3m6eszfSCYEoEeQv0",
+    authDomain: "kwitter-a16a9.firebaseapp.com",
+    databaseURL: "https://kwitter-a16a9-default-rtdb.firebaseio.com",
+    projectId: "kwitter-a16a9",
+    storageBucket: "kwitter-a16a9.appspot.com",
+    messagingSenderId: "401978275152",
+    appId: "1:401978275152:web:4cbb23ecd1a8b81504a855"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
